7c2fbd7fe8ed6d8bfde8afa8dc47a3edea6f266b
Eduardo Santos <santos.eduardothomaz@gmail.com>
2022-07-23 13:20:51 -0300

    test(trie): insert test cases
    test(trie): new insert testcase considering more than one char
    
    test(trie): new insert testcase considering more than one insert
    
    test(trie): new insert testcase considering multiple inserts with multiple key length
    
    test(trie): new insert testcase when there are substrings with value

--- internal/data_structures/trie_test.go
+++ internal/data_structures/trie_test.go
@@ -65,3 +65,134 @@ func TestTrie(t *testing.T) {
 		})
 	}
 }
+
+func TestTrie_Insert(t *testing.T) {
+	type args struct {
+		key   []rune
+		value int
+	}
+
+	type testCase struct {
+		it   string
+		in   []args
+		want *datastructures.TrieNode[rune, int]
+	}
+
+	testCases := []testCase{
+		{
+			it: "should have a terminal children containing rune 't'",
+			in: []args{{[]rune("t"), 0}},
+			want: &datastructures.TrieNode[rune, int]{
+				Children: map[rune]*datastructures.TrieNode[rune, int]{
+					't': {
+						Children:   make(map[rune]*datastructures.TrieNode[rune, int]),
+						IsTerminal: true,
+						Key:        't',
+						Value:      pointy.Int(0),
+					},
+				},
+				IsTerminal: false,
+				Key:        rune(0),
+				Value:      nil,
+			},
+		},
+		{
+			it: "should have a terminal children with keys 't' and 'a'",
+			in: []args{
+				{[]rune("t"), 7},
+				{[]rune("a"), -12},
+			},
+			want: &datastructures.TrieNode[rune, int]{
+				Children: map[rune]*datastructures.TrieNode[rune, int]{
+					't': {
+						Children:   make(map[rune]*datastructures.TrieNode[rune, int]),
+						IsTerminal: true,
+						Key:        't',
+						Value:      pointy.Int(7),
+					},
+					'a': {
+						Children:   make(map[rune]*datastructures.TrieNode[rune, int]),
+						IsTerminal: true,
+						Key:        'a',
+						Value:      pointy.Int(-12),
+					},
+				},
+				IsTerminal: false,
+				Key:        rune(0),
+				Value:      nil,
+			},
+		},
+		{
+			it: "should have a terminal children containing key 'te' and containing key 'ta'",
+			in: []args{
+				{[]rune("te"), 1234},
+				{[]rune("ta"), 4321},
+			},
+			want: &datastructures.TrieNode[rune, int]{
+				Children: map[rune]*datastructures.TrieNode[rune, int]{
+					't': {
+						Children: map[rune]*datastructures.TrieNode[rune, int]{
+							'e': {
+								Children:   make(map[rune]*datastructures.TrieNode[rune, int]),
+								IsTerminal: true,
+								Key:        'e',
+								Value:      pointy.Int(1234),
+							},
+							'a': {
+								Children:   make(map[rune]*datastructures.TrieNode[rune, int]),
+								IsTerminal: true,
+								Key:        'a',
+								Value:      pointy.Int(4321),
+							},
+						},
+						IsTerminal: false,
+						Key:        't',
+						Value:      nil,
+					},
+				},
+				IsTerminal: false,
+				Key:        rune(0),
+				Value:      nil,
+			},
+		},
+		{
+			it: "should have a terminal children for 't' and one for 'te'",
+			in: []args{
+				{[]rune("te"), 1234},
+				{[]rune("t"), 4321},
+			},
+			want: &datastructures.TrieNode[rune, int]{
+				Children: map[rune]*datastructures.TrieNode[rune, int]{
+					't': {
+						Children: map[rune]*datastructures.TrieNode[rune, int]{
+							'e': {
+								Children:   make(map[rune]*datastructures.TrieNode[rune, int]),
+								IsTerminal: true,
+								Key:        'e',
+								Value:      pointy.Int(1234),
+							},
+						},
+						IsTerminal: true,
+						Key:        't',
+						Value:      pointy.Int(4321),
+					},
+				},
+				IsTerminal: false,
+				Key:        rune(0),
+				Value:      nil,
+			},
+		},
+	}
+
+	for _, scenario := range testCases {
+		t.Run(scenario.it, func(t *testing.T) {
+			root := datastructures.NewTrie[rune, int](rune(0), nil)
+			for _, arg := range scenario.in {
+				root.Insert(arg.key, arg.value)
+			}
+
+			assert.Equal(t, scenario.want, root)
+		})
+	}
+
+}
