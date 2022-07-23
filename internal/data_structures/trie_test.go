7afd2d2228525793247694a4f4ee897a81894455
Eduardo Santos <santos.eduardothomaz@gmail.com>
2022-07-23 12:51:06 -0300

    test(trie): find children

--- internal/data_structures/trie_test.go
+++ internal/data_structures/trie_test.go
@@ -18,7 +18,7 @@ func TestTrie(t *testing.T) {
 		"it should find test": {
 			before: func(t *testing.T, tn *datastructures.TrieNode[rune, int]) {
 				test := []rune("test")
-				tn.Insert(test, 2131)
+				tn.Insert(test, 321)
 			},
 			assert: func(t *testing.T, tn *datastructures.TrieNode[rune, int]) {
 				test := []rune("test")
@@ -27,9 +27,27 @@ func TestTrie(t *testing.T) {
 					Children:   make(map[rune]*datastructures.TrieNode[rune, int]),
 					IsTerminal: true,
 					Key:        't',
-					Value:      pointy.Int(2131),
+					Value:      pointy.Int(321),
 				}
 				assert.Equal(t, want, v)
+			},
+		},
+		"it should find subparts of test": {
+			before: func(t *testing.T, tn *datastructures.TrieNode[rune, int]) {
+				test := []rune("test")
+				tn.Insert(test, 1234)
+			},
+			assert: func(t *testing.T, tn *datastructures.TrieNode[rune, int]) {
+				test := []rune("test")
+				for i := range test[:len(test)-1] {
+					actual := tn.Find(test[:i+1])
+					t.Log(test[:i+1])
+					assert.NotEmpty(t, actual)
+					assert.Equal(t, false, actual.IsTerminal)
+					assert.Equal(t, test[i], actual.Key, "asserts node key")
+					assert.Nil(t, actual.Value, "node value is not nil")
+					assert.NotNil(t, actual.Children[test[i+1]], "children is nil")
+				}
 			},
 		},
 	}
